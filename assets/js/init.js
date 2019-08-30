56775194e89a6ea02b1a6e3ee68cca01fb198c34
hanasari <Calvin@Daejongui-iMac.local>
2019-08-30 16:59:56 +0900

    rearrange source

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,25 +1,20 @@
 
- 
- (function($){
-   $(function(){
-    // M.AutoInit();
+(function($){
+  $(function(){
+    $('.button-collapse').sidenav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.materialboxed').materialbox();
+    $('.scrollspy').scrollSpy();
 
-     $('.button-collapse').sidenav();
-     $('.parallax').parallax();
-     $('.collapsible').collapsible();
-     $('.carousel.carousel-slider').carousel({fullWidth: true});
-     $('.materialboxed').materialbox();
-     $('.scrollspy').scrollSpy();
-    //  $('.tap-target').tapTarget('open');
+    if (localStorage.getItem('cookieconsent') === 'true') {
+      $('#cookies').hide()
+    }
 
-    //  if (localStorage.getItem('cookieconsent') === 'true') {
-    //    $('#cookies').hide()
-    //  }
-
-    //  jQuery('#cookies').on('click', function(event) {
-    //         localStorage.setItem('cookieconsent', 'true')
-    //         jQuery('#cookies').toggle('hide');
-    //    });
-
-   }); // end of document ready
- })(jQuery);
+    jQuery('#cookies').on('click', function(event) {
+      localStorage.setItem('cookieconsent', 'true')
+      jQuery('#cookies').toggle('hide');
+    });
+  });
+})(jQuery);
